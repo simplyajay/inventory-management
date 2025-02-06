0c02469cc133b64bbbe58078a990b0037905824b
Aldriahn Jay <aldriahnjayjamio@gmail.com>
2025-02-06 20:53:47 +0400

    refractored table resizing

--- frontend/src/components/table/Table.jsx
+++ frontend/src/components/table/Table.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { use, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState, useRef } from "react";
 import { MoonLoader } from "react-spinners";
 import { CaretDown, CaretUp } from "../icons/Icons";
 
@@ -20,8 +20,92 @@ const Table = ({
   const headingClass =
     "px-3 py-2 text-md font-extralight border border-dotted border-gray-300 sticky top-0 bg-background shadow-sm";
 
-  const bodyClass =
-    "p-2 border border-dotted border-gray-300 whitespace-nowrap ";
+  const bodyClass = "p-2 border border-dotted border-gray-300 whitespace-nowrap ";
+
+  const tableRef = useRef(null);
+  const headerRefs = useRef([]);
+  const resizeRef = useRef(false);
+  const [columnWidths, setColumnWidths] = useState({});
+
+  let startX, startWidth;
+  const offset = 10;
+
+  const handleMouseMove = useCallback((e) => {
+    headerRefs.current.forEach((header) => {
+      const rect = header.getBoundingClientRect();
+
+      if (e.pageX > rect.right - offset) {
+        header.style.cursor = "col-resize";
+      } else {
+        header.style.cursor = "pointer";
+      }
+    });
+  });
+
+  const handleMouseDown = useCallback((e) => {
+    const header = e.currentTarget;
+    const rect = header.getBoundingClientRect();
+    const index = headerRefs.current.indexOf(header);
+
+    if (e.pageX > rect.right - offset) {
+      resizeRef.current = true;
+      startX = e.pageX;
+      startWidth = header.offsetWidth;
+      let newWidth;
+
+      const handleResize = (x) => {
+        if (!resizeRef.current) return;
+
+        const tableRect = tableRef.current.getBoundingClientRect();
+        const clampedX = Math.min(x.pageX, tableRect.right);
+        newWidth = Math.max(50, startWidth + (clampedX - startX));
+      };
+
+      const stopResize = () => {
+        setColumnWidths((prev) => ({ ...prev, [index]: newWidth }));
+        setTimeout(() => {
+          resizeRef.current = false; // Delay to allow click event to process
+        }, 50);
+        document.removeEventListener("mousemove", handleResize);
+        document.removeEventListener("mouseup", stopResize);
+      };
+
+      document.addEventListener("mousemove", handleResize);
+      document.addEventListener("mouseup", stopResize);
+    }
+  });
+
+  useEffect(() => {
+    headerRefs.current.forEach((headerRef) => {
+      if (!headerRef) return;
+
+      headerRef.addEventListener("mousemove", handleMouseMove);
+      headerRef.addEventListener("mousedown", handleMouseDown);
+    });
+
+    return () => {
+      headerRefs.current.forEach((headerRef) => {
+        if (!headerRef) return;
+
+        headerRef.removeEventListener("mousemove", handleMouseMove);
+        headerRef.removeEventListener("mousedown", handleMouseDown);
+      });
+    };
+  }, [handleMouseDown, handleMouseMove]);
+
+  const handleHeaderclick = (header) => {
+    if (resizeRef.current) return;
+
+    if (handleSort && sortSetting) {
+      const type =
+        sortSetting.key === header.key ? (sortSetting.type === "asc" ? "desc" : "asc") : "asc";
+      handleSort({
+        key: header.key,
+        type,
+        keyword: sortSetting.searchKeyword,
+      });
+    }
+  };
 
   return (
     <div className="w-full h-full overflow-auto select-none">
@@ -31,6 +115,7 @@ const Table = ({
         </div>
       ) : (
         <table
+          ref={tableRef}
           className={`${
             bodies.length === 0 ? "h-full" : ""
           } w-full table-auto border-collapse border-spacing-0 scroll-smooth`}
@@ -39,43 +124,23 @@ const Table = ({
             <tr>
               {headers.map((header, index) => (
                 <th
+                  ref={(el) => (headerRefs.current[index] = el)}
                   key={index}
-                  className={`${headingClass} cursor-pointer`}
-                  onClick={() => {
-                    if (handleSort && sortSetting) {
-                      const type =
-                        sortSetting.key === header.key
-                          ? sortSetting.type === "asc"
-                            ? "desc"
-                            : "asc"
-                          : "asc";
-                      handleSort({
-                        key: header.key,
-                        type,
-                        keyword: sortSetting.searchKeyword,
-                      });
-                    }
-                  }}
+                  style={{ width: columnWidths[index] ? `${columnWidths[index]}px` : "auto" }}
+                  className={`${headingClass}`}
+                  onClick={() => handleHeaderclick(header)}
                 >
                   <div className="flex gap-2 items-center">
                     <span>{header.name}</span>
                     {sortSetting && sortSetting.key === header.key ? (
-                      <span>
-                        {sortSetting.type === "asc" ? (
-                          <CaretUp />
-                        ) : (
-                          <CaretDown />
-                        )}
-                      </span>
+                      <span>{sortSetting.type === "asc" ? <CaretUp /> : <CaretDown />}</span>
                     ) : (
                       <></>
                     )}
                   </div>
                 </th>
               ))}
-              {actions?.header && (
-                <th className={headingClass}>{actions.header}</th>
-              )}
+              {actions?.header && <th className={headingClass}>{actions.header}</th>}
             </tr>
           </thead>
           <tbody className="w-full">
@@ -96,9 +161,7 @@ const Table = ({
                     {actions?.components && (
                       <td className={`${bodyClass} flex gap-1 justify-center`}>
                         {actions.components.map((component, index) => (
-                          <div key={index}>
-                            {React.cloneElement(component, { target: body })}
-                          </div>
+                          <div key={index}>{React.cloneElement(component, { target: body })}</div>
                         ))}
                       </td>
                     )}
@@ -109,9 +172,7 @@ const Table = ({
               <tr>
                 <td colSpan={actions ? headers.length + 1 : headers.length}>
                   <div className="flex justify-center items-center h-full italic text-2xl">
-                    <p className="text-gray-500 select-none">
-                      {messageWhenEmpty}
-                    </p>
+                    <p className="text-gray-500 select-none">{messageWhenEmpty}</p>
                   </div>
                 </td>
               </tr>
