335c7988951c4e4d4b02cf9aae4f213ab972795c
JayOff <aldriahnjayjamio@gmail.com>
2025-02-07 15:19:04 +0400

    frontend supplier updates

--- frontend/src/app/suppliers/components/Layout.jsx
+++ frontend/src/app/suppliers/components/Layout.jsx
@@ -18,16 +18,32 @@ const Layout = () => {
   const handleSort = () => {};
 
   return (
-    <div className="w-full h-full border border-red-500 p-5">
-      <Table
-        loading={loading}
-        headers={tableHeaders}
-        bodies={tableBodies}
-        actions={tableActions}
-        messageWhenEmpty="You don't have any suppliers"
-        sortSetting={{ ...sortBy, searchKeyword }}
-        handleSort={handleSort}
-      />
+    <div className="w-full h-full p-4">
+      <div className="w-full h-full flex flex-col shadow-md p-2">
+        <div className="w-full p-2 flex justify-between">
+          <div className="flex items-center justify-center">Suppliers</div>
+          <div className="flex h-full flex-row-reverse items-end">
+            <button
+              id="addButton"
+              type="button"
+              className="w-full border border-gray-500 rounded-lg p-2 select-none"
+            >
+              Add Supplier
+            </button>
+          </div>
+        </div>
+        <div className="w-full h-full overflow-hidden">
+          <Table
+            loading={loading}
+            headers={tableHeaders}
+            bodies={tableBodies}
+            actions={tableActions}
+            messageWhenEmpty="You don't have any suppliers"
+            sortSetting={{ ...sortBy, searchKeyword }}
+            handleSort={handleSort}
+          />
+        </div>
+      </div>
     </div>
   );
 };
