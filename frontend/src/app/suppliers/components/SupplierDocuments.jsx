16c12cc268f7294b685a08780af799fbfd4c088e
Aldriahn Jay <aldriahnjayjamio@gmail.com>
2025-03-21 20:32:53 +0400

    created supplier's document component

--- frontend/src/app/suppliers/components/SupplierDocuments.jsx
+++ frontend/src/app/suppliers/components/SupplierDocuments.jsx
@@ -1,5 +1,10 @@
 "use client";
 import React, { useState, useRef } from "react";
+import TableLayout from "@/components/table/TableLayout";
+import TableHead from "@/components/table/TableHead";
+import Table from "@/components/table/Table";
+import Pagination from "@/components/table/Pagination";
+import { createTableHandler } from "@/components/table/table.util";
 
 const SupplierDocuments = () => {
   const [state, setState] = useState({
@@ -10,6 +15,11 @@ const SupplierDocuments = () => {
     page: 1,
     totalPages: 0,
   });
+
+  const tableHeaders = [];
+  const tableActions = [];
+
+  const { documents, loading, searchKeyword, sortBy, page, totalPages } = state;
 
   const updateState = (updates) => {
     setState((prev) => ({ ...prev, ...updates }));
@@ -22,7 +32,45 @@ const SupplierDocuments = () => {
     searchKeyword = "",
     sortBy = { key: "name", type: "asc" },
   } = {}) => {};
-  return <div className="w-full h-full bg-white">SupplierDocuments</div>;
+
+  const handleTableButtonClick = () => {
+    console.log("test");
+  };
+
+  const { pageNext, pagePrev, searchItem, clearSearch, handleSort } = createTableHandler({
+    state,
+    fetchTableData: fetchDocuments,
+  });
+  return (
+    <div className="w-full h-full">
+      <TableLayout loading={loading}>
+        <TableHead
+          title={"Documents"}
+          buttonText={"New Document"}
+          onButtonClick={handleTableButtonClick}
+          searchRef={searchRef}
+          handleSearch={searchItem}
+          handleSearchClear={clearSearch}
+          searchKeyword={searchKeyword}
+        />
+        <Table
+          loading={loading}
+          headers={tableHeaders}
+          bodies={documents}
+          actions={tableActions}
+          messageWhenEmpty="There are no documents"
+          sortSetting={{ ...sortBy, searchKeyword }}
+          handleSort={handleSort}
+        />
+        <Pagination
+          onPrevPage={() => pagePrev(searchKeyword)}
+          onNextPage={() => pageNext(searchKeyword)}
+          currentPage={page}
+          totalPages={totalPages}
+        />
+      </TableLayout>
+    </div>
+  );
 };
 
 export default SupplierDocuments;
